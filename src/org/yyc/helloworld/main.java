ef996e5fb7122cf269214811c465f6db4139f806
Aquarius <1009181519@qq.com>
2017-09-30 13:53:20 +0800

    fb

---
+++ src/org/yyc/helloworld/main.java
@@ -0,0 +1,14 @@
+package org.yyc.helloworld;
+
+/**
+ * Created by idea on 2017/9/30.
+ */
+public class main {
+    public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ test/org/yyc/helloworld/mainTest.java
@@ -0,0 +1,16 @@
+package org.yyc.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by idea on 2017/9/30.
+ */
+public class mainTest {
+    @Test
+    public void hello() throws Exception {
+        main.hello();
+    }
+
+}
